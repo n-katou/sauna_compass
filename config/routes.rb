245cad2942bae763635187cad7471b86cd3dfc3c
NaotoKato <102382177+n-katou@users.noreply.github.com>
2023-12-18 08:08:21 +0000

    認証機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+   devise_for :admins, controllers: {
+     sessions: 'admin/sessions'
+   }
+   devise_for :customers, controllers: {
+     sessions: 'customer/sessions',
+     registrations: 'customer/registrations'
+   }
+   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
