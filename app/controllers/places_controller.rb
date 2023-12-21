9583204f67e5f2c87f38161f5ee88260670b6bb4
NaotoKato <102382177+n-katou@users.noreply.github.com>
2023-12-21 06:18:09 +0000

    placeのCRUD機能実装

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,50 @@
+class PlacesController < ApplicationController
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @places = Place.all
+  end
+
+  def show
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @place.update(place_params)
+      redirect_to @place
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to places_path, notice: 'Place was successfully destroyed.'
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :address, :rating_avg, :longitude, :latitude, :phone_number, :opening_hours, :website)
+  end
+end
